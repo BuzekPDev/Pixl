42b7683c271927e363873327252949318543e0e8
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2025-03-22 00:26:19 +0100

    rgba difference calculated correctly now

--- src/classes/ColorProcessor.ts
+++ src/classes/ColorProcessor.ts
@@ -6,15 +6,36 @@ export type RGBA = [number, number, number, number]
 
 export class ColorProcessor {
 
-  getRGBDifference (originalColor: RGBA, newColor: RGBA) {
+  getRGBDifference (originalColor: RGBA, newColor: RGBA): RGBA {
     // I can just subtract the numbers lmao
+    
+    return [
+      originalColor[0] - newColor[0],
+      originalColor[1] - newColor[1],
+      originalColor[2] - newColor[2],
+      originalColor[3] - newColor[3],
+    ] 
+  }
 
+  applyRGBADifference (color: RGBA, difference: RGBA) {
+    const newColor = [
+      color[0] - difference[0],
+      color[1] - difference[1],
+      color[2] - difference[2],
+      color[3] - difference[3],
+    ]
     return newColor
   }
 
-  applyRGBADifference (color: RGBA, difference: RGBA) {}
-
-  revertRGBADifference (color: RGBA, difference: RGBA) {}
+  revertRGBADifference (color: RGBA, difference: RGBA) {
+    const newColor = [
+      color[0] + difference[0],
+      color[1] + difference[1],
+      color[2] + difference[2],
+      color[3] + difference[3],
+    ]
+    return newColor
+  }
 
   parseRGBAString (str: string) {}
 

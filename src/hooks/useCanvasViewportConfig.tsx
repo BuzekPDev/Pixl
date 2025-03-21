a61698830dcbbbdc41e7e6203d7f3232140eff58
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2025-03-21 23:15:26 +0100

    calculate scale during canvas setup

--- src/hooks/useCanvasViewportConfig.tsx
+++ src/hooks/useCanvasViewportConfig.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react"
 
+export interface CanvasDimensions {
+  width: number,
+  height: number,
+  scaleX: number,
+  scaleY: number
+}
 
 export const useCanvasViewportConfig = () => {
 
-  const [dimensions, setDimensions] = useState({
+  const [dimensions, setDimensions] = useState<CanvasDimensions>({
     width: 0,
-    height: 0
+    height: 0,
+    scaleX: 1,
+    scaleY: 1
   })
 
 
@@ -13,7 +21,13 @@ export const useCanvasViewportConfig = () => {
     dimensions: {
       width: dimensions.width,
       height: dimensions.height,
-      set: (width: number, height: number) => setDimensions({width, height})
+      scaleX: dimensions.scaleX,
+      scaleY: dimensions.scaleY,
+      set: (width: number, height: number, canvasWidth: number, canvasHeight: number) => {
+        const scaleX = canvasWidth / width;
+        const scaleY = canvasHeight / height;
+        setDimensions({ width, height, scaleX, scaleY })
+      }
     }
   }
 }

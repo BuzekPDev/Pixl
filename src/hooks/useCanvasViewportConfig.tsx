88d426f7c232004440b491ba2ce4295620324321
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2025-03-22 17:53:37 +0100

    viewportConfig types

--- src/hooks/useCanvasViewportConfig.tsx
+++ src/hooks/useCanvasViewportConfig.tsx
@@ -1,10 +1,25 @@
 import { useState } from "react"
 
+export interface CanvasViewportConfig {
+  dimensions: CanvasDimensionsController;
+}
+
 export interface CanvasDimensions {
   width: number,
   height: number,
   scaleX: number,
-  scaleY: number
+  scaleY: number,
+  aspectRatio: number
+}
+
+export interface CanvasDimensionsController extends CanvasDimensions {
+  set: (
+    width: number,
+    height: number,
+    canvasWidth: number,
+    canvasHeight: number,
+    aspectRatio: number
+  ) => void
 }
 
 export const useCanvasViewportConfig = () => {
@@ -13,21 +28,25 @@ export const useCanvasViewportConfig = () => {
     width: 0,
     height: 0,
     scaleX: 1,
-    scaleY: 1
+    scaleY: 1,
+    aspectRatio: 1
   })
 
 
   return {
     dimensions: {
-      width: dimensions.width,
-      height: dimensions.height,
-      scaleX: dimensions.scaleX,
-      scaleY: dimensions.scaleY,
-      set: (width: number, height: number, canvasWidth: number, canvasHeight: number) => {
+      ...dimensions,
+      set: (
+        width: number,
+        height: number,
+        canvasWidth: number,
+        canvasHeight: number,
+        aspectRatio: number
+      ) => {
         const scaleX = canvasWidth / width;
-        const scaleY = canvasHeight / height;
-        setDimensions({ width, height, scaleX, scaleY })
-      }
+        const scaleY = canvasHeight / (height * aspectRatio);
+        setDimensions({ width, height, scaleX, scaleY, aspectRatio })
+      },
     }
   }
 }

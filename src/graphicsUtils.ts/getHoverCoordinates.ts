208affdd2fd265ab89d6da9981ecd8064f438984
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2025-03-22 17:53:22 +0100

    adjusted to return true coordinates instead of scaled ones

--- src/graphicsUtils.ts/getHoverCoordinates.ts
+++ src/graphicsUtils.ts/getHoverCoordinates.ts
@@ -1,23 +1,20 @@
 import { CanvasDimensions } from "../hooks/useCanvasViewportConfig"
 
 export const getHoverCoordinates = (clientX: number, clientY: number, dimensions: CanvasDimensions, ctx: CanvasRenderingContext2D, toolSize: ToolSize) => {
-  const {scaleX, scaleY} = dimensions;
+  const { aspectRatio } = dimensions;
   const { left, top, width, height } = (ctx.canvas as HTMLCanvasElement).getBoundingClientRect()
 
   const relativeClientX = Math.floor((clientX - left) * (dimensions.width / width))
-  const relativeClientY = Math.floor((clientY - top) * (dimensions.height / height))
+  const relativeClientY = Math.floor((clientY - top) * ((dimensions.height) / height))
 
-  const x = Math.floor(relativeClientX-Math.floor(toolSize/2)) * scaleX;
-  const y = Math.floor(relativeClientY-Math.floor(toolSize/2)) * scaleY;
-
-  const toolSizeX = toolSize * scaleX
-  const toolSizeY = toolSize * scaleY;
+  const x = Math.floor(relativeClientX-Math.floor(toolSize/2)) 
+  const y = Math.floor((relativeClientY / aspectRatio)-Math.floor(toolSize/2))
 
   return {
     x,
     y,
-    toolSizeX,
-    toolSizeY
+    toolSizeX: toolSize,
+    toolSizeY: toolSize
   }
 }
 

4133f26265cd59e1c30d27aa35fc5f73cc769113
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2025-03-25 10:21:26 +0100

    drawing area size scaling

--- src/hooks/useCanvasViewportConfig.tsx
+++ src/hooks/useCanvasViewportConfig.tsx
@@ -12,7 +12,7 @@ export interface CanvasDimensionsController {
 export interface CanvasDimensions {
   viewport: Dimensions;
   size: Dimensions;
-  position: Position; 
+  position: Position;
   resolution: Dimensions;
   aspectRatio: number;
   zoom: number
@@ -29,12 +29,12 @@ export interface Position {
 }
 
 export const useCanvasViewportConfig = () => {
-  
+
   const dimensions = useRef<CanvasDimensions>({
-    viewport: {width: 0, height: 0},
-    size: {width: 0, height: 0},
-    position: {x: 0, y: 0},
-    resolution: {width: 0, height: 0},
+    viewport: { width: 0, height: 0 },
+    size: { width: 0, height: 0 },
+    position: { x: 0, y: 0 },
+    resolution: { width: 0, height: 0 },
     aspectRatio: 1,
     zoom: 1
   })
@@ -44,12 +44,20 @@ export const useCanvasViewportConfig = () => {
       ref: dimensions,
       set: <K extends keyof CanvasDimensions>(dims: Record<K, CanvasDimensions[K]>) => {
         (Object.entries(dims) as Array<[K, CanvasDimensions[K]]>)
-        .forEach(([key, val]) => {
-          dimensions.current[key] = val
-        }) 
+          .forEach(([key, val]) => {
+            dimensions.current[key] = val
+          })
         if ((dims as CanvasDimensions)?.resolution) {
-          const {width, height} = (dims as CanvasDimensions).resolution
-          dimensions.current.aspectRatio = height / width
+          const { width, height } = (dims as CanvasDimensions).resolution
+          const aspectRatio = height / width 
+          dimensions.current.aspectRatio = aspectRatio
+
+          const scale = 4
+
+          dimensions.current.size = {
+            width: width*scale,
+            height: height*scale
+          }
         }
       }
     }

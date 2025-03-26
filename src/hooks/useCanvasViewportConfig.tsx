e062f530aeb7fdece3f032e96c400d9a146fcb9c
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2025-03-26 09:43:41 +0100

    recenter & resclae fn

--- src/hooks/useCanvasViewportConfig.tsx
+++ src/hooks/useCanvasViewportConfig.tsx
@@ -15,7 +15,8 @@ export interface CanvasDimensions {
   position: Position;
   resolution: Dimensions;
   aspectRatio: number;
-  zoom: number
+  zoom: number;
+  scale: number;
 }
 
 export interface Dimensions {
@@ -36,8 +37,40 @@ export const useCanvasViewportConfig = () => {
     position: { x: 0, y: 0 },
     resolution: { width: 0, height: 0 },
     aspectRatio: 1,
-    zoom: 1
+    zoom: 1,
+    scale: 1,
   })
+
+  const rescale = () => {
+    const { resolution, scale } = dimensions.current
+    const dpr = window.devicePixelRatio
+    const width = Math.max(resolution.width * scale, resolution.width / dpr)
+    const height = Math.max(resolution.height * scale, resolution.height / dpr)
+
+    dimensions.current.size = {
+      width: width,//dimensions.current.resolution.width*dimensions.current.scale,
+      height: height//dimensions.current.resolution.height*dimensions.current.scale
+    }
+  }
+
+  const recenter = () => {
+    const { resolution, scale, viewport } = dimensions.current
+    const dpr = window.devicePixelRatio
+    const width = Math.max(resolution.width * scale, resolution.width / dpr)
+    const height = Math.max(resolution.height * scale, resolution.height / dpr)
+
+    if (viewport.width > viewport.height) {
+      dimensions.current.position = {
+        x: (viewport.width - width) / 2,
+        y: 0
+      }
+    } else {
+      dimensions.current.position = {
+        x: 0,
+        y: (viewport.height - height) / 2 
+      }
+    }
+  }
 
   return {
     dimensions: {
@@ -49,17 +82,28 @@ export const useCanvasViewportConfig = () => {
           })
         if ((dims as CanvasDimensions)?.resolution) {
           const { width, height } = (dims as CanvasDimensions).resolution
-          const aspectRatio = height / width 
-          dimensions.current.aspectRatio = aspectRatio
+          const viewportAspectRatio = dimensions.current.viewport.height / dimensions.current.viewport.width
 
-          const scale = 4
+          dimensions.current.aspectRatio = viewportAspectRatio
+          const { resolution, scale } = dimensions.current
 
-          dimensions.current.size = {
-            width: width*scale,
-            height: height*scale
-          }
+          const scaleX = Math.round((dimensions.current.viewport.width / width) * 100) / 100// / ar
+          const scaleY = Math.round((dimensions.current.viewport.height / height) * 100) / 100
+
+          dimensions.current.scale = Math.max(Math.min(scaleX, scaleY), 1)
+
+          // const dpr = window.devicePixelRatio
+
+          // dimensions.current.size = {
+          //   width: Math.max(resolution.width * scale, resolution.width / dpr), 
+          //   height: Math.max(resolution.height * scale, resolution.height / dpr)//height//dimensions.current.resolution.height*dimensions.current.scale
+          // }
         }
-      }
+
+        rescale()
+      },
+      rescale,
+      recenter
     }
   }
 }

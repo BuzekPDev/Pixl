8d0071069a15ba24c4c650381ab19990b0e1330d
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2025-03-31 15:33:33 +0200

    removed main buffer

--- src/hooks/useOffscreenBuffer.tsx
+++ src/hooks/useOffscreenBuffer.tsx
@@ -2,14 +2,10 @@ import { useMemo } from "react";
 
 export interface OffscreenBuffer {
   transparencyGridBuffer: OffscreenCanvasRenderingContext2D;
-  drawingBuffer: OffscreenCanvasRenderingContext2D;
   hoverOverlayBuffer: OffscreenCanvasRenderingContext2D;
 }
 
 export const useOffscreenBuffer = () => {
-  const drawingBuffer = useMemo(() =>
-    new OffscreenCanvas(0, 0).getContext("2d"),
-  []);
 
   const transparencyGridBuffer = useMemo(() =>
     new OffscreenCanvas(0, 0).getContext("2d"),
@@ -20,8 +16,7 @@ export const useOffscreenBuffer = () => {
   []);
 
   return {
-    drawingBuffer,
     transparencyGridBuffer,
-    hoverOverlayBuffer
+    hoverOverlayBuffer,
   }
 }

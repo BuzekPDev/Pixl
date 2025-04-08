8d09fb9ba5c0699ae4457de99f99bcbf6f5021bb
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2025-04-08 15:56:09 +0200

    move mobile sidebar toggles to header from toolbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,62 +1,61 @@
-import { useCallback, useEffect, useState } from "react"
-import { useCanvasApi } from "../context/canvasContext2d"
-import { debounce } from "../utils/debounce"
 import { icons } from "../icons"
 import Logo from "../assets/Pixl_logo.png"
 import { ToolButton } from "./ToolButton"
 
-export const Header = ({ fileName, changeFileName }: { fileName: string, changeFileName: (e) => void }) => {
+export const Header = ({
+  fileName,
+  changeFileName,
+  openPanel,
+  panelState
+}: {
+  fileName: string,
+  changeFileName: (e: React.ChangeEvent<HTMLInputElement>) => void,
+  openPanel: (panel: "tools" | "tabbed") => void;
+  panelState: {tools: boolean, tabbed: boolean}
+}) => {
 
-  const { viewportManager, canvasController, frameManager, importFile, importGif } = useCanvasApi()
 
   return (
     <header
-      className="grid grid-cols-3 absolute h-16 w-full top-0 left-0 bg-neutral-800"
+      className="grid lg:grid-cols-[200px_1fr_200px] grid-cols-[80px_1fr_80px] w-full lg:h-16 h-14 px-4 top-0 left-0 bg-neutral-800 absolute z-70"
     >
-
-      <div className="flex items-center gap-4 ml-5">
-        <img className="h-10 hidden md:block" src={Logo} />
-      </div>
-      <div className="flex justify-center items-center fill-white relative">
-        <div className="flex relative"> 
-        <input
-          className="appearance-none pr-6 text-lg text-white text-center outline-0 border-b border-transparent focus:border-b-neutral-400"
-          type="text"
-          placeholder="New Pixl project"
-          style={{ width: `${Math.max(fileName.length+5, 18)}ch` }}
-          value={fileName}
-          maxLength={40}
-          onChange={changeFileName}
-        />
-        <div className="absolute right-0 bottom-1/2 translate-y-1/2 pointer-events-none">
-          {icons.pencil}
-        </div>
+      <div className="flex items-center gap-4">
+        <img className="h-10 hidden lg:block" src={Logo} />
+        <div className="lg:hidden">
+          <ToolButton
+            onClick={() => openPanel("tools")}
+            isSelected={panelState.tools}
+          >
+            {icons.brushAndPalette}
+          </ToolButton>
         </div>
       </div>
-      <div className="w-10 h-10"></div>
-      {/* <input
-        type="number"
-        value={dims.width}
-        onChange={(e) => {
-          const width = parseInt(e.target.value)
-          setDims(d => ({ ...d, width }))
-          widthResizeDebounce(width)
-        }}
-      />
-      <input
-        type="number"
-        value={dims.height}
-        onChange={(e) => {
-          const height = parseInt(e.target.value)
-          console.debug(height)
-          setDims(d => ({ ...d, height }))
-          heightResizeDebounce(height)
-        }}
-      />
-      <button onClick={() => frameManager.exportAsGif()}>download gif</button>
-      <button onClick={() => frameManager.exportAsImage("png", "shinjer")}>download png</button>
-      <button onClick={() => frameManager.exportAsImage("jpeg", "shinjer")}>download jpeg</button>
-       */}
+      <div className="flex justify-center items-center grow-0 fill-white relative overflow-hidden">
+        <div className="flex relative">
+          <input
+            className="appearance-none pr-6 text-lg text-white text-center outline-0 border-b border-transparent focus:border-b-neutral-400"
+            type="text"
+            placeholder="New Pixl project"
+            style={{ width: `${Math.max(fileName.length + 5, 18)}ch` }} // +5 to make up for padding and prevent hidden overflow
+            value={fileName}
+            maxLength={40}
+            onChange={changeFileName}
+          />
+          <div className="absolute right-0 bottom-1/2 translate-y-1/2 pointer-events-none">
+            {icons.pencil}
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center place-self-end w-fit h-full">
+        <div className="lg:hidden">
+          <ToolButton
+            onClick={() => openPanel("tabbed")}
+            isSelected={panelState.tabbed}
+          >
+            {icons.frames}
+          </ToolButton>
+        </div>
+      </div>
     </header>
   )
 }

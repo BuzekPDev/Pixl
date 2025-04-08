81d66f7df3451f82190bd73ab8343e78b7e85c5f
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2025-04-08 15:58:38 +0200

    mobile-only taskbar for undo/redo

---
+++ src/components/Taskbar.tsx
@@ -0,0 +1,25 @@
+import { icons } from "../icons"
+import { ToolButton } from "./ToolButton"
+import logo from "../assets/Pixl_logo.png"
+import { useCanvasApi } from "../context/canvasContext2d"
+
+export const Taskbar = () => {
+
+  const { toolsController } = useCanvasApi()
+
+  return (
+    <div className="flex items-center justify-between px-4 w-full h-14 bg-neutral-800 absolute bottom-0 lg:hidden">
+      <ToolButton
+        onClick={toolsController.undo}
+      >
+        {icons.undo}
+      </ToolButton>
+      <img className="h-10" src={logo}/>
+      <ToolButton
+        onClick={toolsController.redo}
+      >
+        {icons.redo}
+      </ToolButton>
+    </div>
+  )
+}
